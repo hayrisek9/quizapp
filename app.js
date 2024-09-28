6fbbc1c367f5973efd28c93dcea299e59bb9a607
hayrisek9 <hayrisek9@gmail.com>
2024-09-28 13:44:24 +0200

    the project finish

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const againBtn = document.querySelector('.again')
 const resault = document.querySelector('.resault')
 const questionElement = document.querySelector('.question');
 const optionsElement = document.querySelectorAll('.option');
+const resaultCorrect = document.querySelector('.resault-correct');
+const resaultWrong = document.querySelector('.resault-wrong');
+const resaultEmpty = document.querySelector('.resault-empty');
 let correctAnswerTotal = 0;
 let wrongAnswerTotal = 0;
 let emptyAnswerTotal = 0;
 let canSelectOption = true;
 let questionNumber = 0
+let interval, sayac, time = 30;
 
 
 
@@ -51,6 +55,7 @@ const questions = [
       options: ['Tokyo', 'Delhi', 'Mumbai' ],
       answer: 'Mumbai' }   
 ]
+
 optionsElement.forEach ((option) => {
    option.addEventListener('click', () => {
     if(!canSelectOption){
@@ -62,11 +67,14 @@ optionsElement.forEach ((option) => {
         if(el.textContent == selectedOption){
             if(selectedOption == correctAnswer){
                 el.style.backgroundColor = '#AACB73'
+                correctAnswerTotal++;
             }else{
                 el.style.backgroundColor = '#A04747'
+                wrongAnswerTotal++;
             }
         }else{
             el.style.backgroundColor = 'transparent'
+           
         }
     })
 canSelectOption = false;
@@ -78,4 +86,77 @@ setTimeout(() =>{
    })
 
 })
+
+function start(){
+startTimerLine();
+clearInterval(sayac)
+document.querySelector('.container').style.opacity = '1';
+document.querySelector('.container').style.pointerEvents = 'all';
 resault.style.display = 'none';
+time = 30 
+timeElement.textContent = time + ` s`;
+timeControl()
+countElement.textContent = `${questionNumber + 1}/ ${questions.length}`;
+optionsElement.forEach((option, index)=>{
+   option.style.backgroundColor = 'transparent';
+   option.textContent = questions[questionNumber].options[index];
+})
+
+questionElement.textContent = questions[questionNumber].question;
+}
+function end(){
+clearInterval(sayac);
+clearInterval(interval);
+document.querySelector('.container').style.opacity = '0.5';
+document.querySelector('.container').style.pointerEvents = 'none';
+resault.style.display = 'block';
+resaultCorrect.textContent = `Correct: ${correctAnswerTotal}`;
+resaultWrong.textContent = `Wrong: ${wrongAnswerTotal}`;
+resaultEmpty.textContent = `Empty: ${emptyAnswerTotal}`;
+}
+
+function nextQuestion(){
+  if(questionNumber < questions.length - 1) {
+    questionNumber++;
+  }else{
+    end();
+    return;
+  }
+ start()
+}
+
+
+function startTimerLine(){
+  timerEement.style.width = "0px";
+  clearInterval(interval);
+
+  const targetWidth = 559;
+  const totalTime = 30000;
+  let currentTime = totalTime
+  interval = setInterval(() =>{
+currentTime -= 10;
+if(currentTime >= 0){
+  const progress = (totalTime - currentTime) / totalTime;
+  const currentWidth = progress * targetWidth ;
+  timerEement.style.width = `${currentWidth}px`;
+}else{
+  clearInterval(interval)
+}
+
+  }, 10)
+}
+function timeControl(){
+sayac = setInterval(() => {
+  if(time > 0){
+    time -= 1;
+    timeElement.textContent = time + ` s` ;
+  }else{
+    emptyAnswerTotal ++;
+    nextQuestion();
+  }
+}, 1000);
+}
+againBtn.addEventListener("click",() =>{
+  window.location.reload();
+})
+start();
